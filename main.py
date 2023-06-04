b1afe498033b18a24693dda5d2d65c6ea53c14a6
motoemoto47ark123 <121888254+motoemoto47ark123@users.noreply.github.com>
2023-06-04 15:42:44 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import re
+
+file_path = "links.txt"  # Path to the text file containing the download links
+output_file = "links-output.txt"  # Path to the output file
+
+pattern = r"https?://[^\s'\"]+"  # Regular expression pattern to match the download links
+
+with open(file_path, "r") as file, open(output_file, "w") as output:
+    for line in file:
+        matches = re.findall(pattern, line)
+        for match in matches:
+            if "" not in match:
+                print(match)
+                output.write(match + "\n")
+
+# Close the output file
+output.close()
